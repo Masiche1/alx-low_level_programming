e883d365193c46ba9acd58f3d63593873776cbe6
Masiche1 <lichalameck@gmail.com>
2022-09-08 17:51:05 -0700

    Prints a string using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string without using printf()
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	Return (0)
+}
