d3404fb903d8eae467a1ffda9f4b586fdeac2b4c
Masiche1 <lichalameck@gmail.com>
2022-09-08 18:17:14 -0700

    Prints a string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string or art using printf
+ *
+ * Returns 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
