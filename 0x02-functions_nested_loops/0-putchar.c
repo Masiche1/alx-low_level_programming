8b1e0d0be95c4cbdbf4df5d09a921371bf6cc281
Masiche1 <lichalameck@gmail.com>
2022-09-14 01:03:36 -0700

    Print a string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Prints _putchar
+ * Return: Always 0
+ */
+void _putchar(char c)
+{
+	return (0);
+}
+int main(void)
+{
+	char c = "_putchar"
+	_putchar(c);
+}
