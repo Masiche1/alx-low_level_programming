861344185a509cb207d3be5a0c19ae304744630d
Masiche1 <lichalameck@gmail.com>
2022-09-16 18:40:30 -0700

    Infinite loop avoided

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
 * main - causes an infinite loop
-* Return: 0
+* Return: Always 0
 */
 
 int main(void)
@@ -13,7 +13,7 @@ int main(void)
 
         i = 0;
 
-/**    while (i < 10)*/
+/**    while (i < 10) */
         {
                 putchar(i);
         }
