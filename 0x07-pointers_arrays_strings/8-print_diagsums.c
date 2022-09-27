ac18fe33f20367652592dfb6bd4480d0ee163b86
Masiche1 <lichalameck@gmail.com>
2022-09-26 18:38:27 -0700

    Sturbon

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,9 +9,7 @@
  */                                                                                                                                       
 void print_diagsums(int *a, int size)
 {
-	int i, j;
-	sum1 = 0;
-	sum2 = 0;
+	int i, j, sum1 = 0, sum2 = 0;
 
 	for (i = size - 1, j = 0; j < (size * size); j+= size + 1, i = size - 1)
 		sum1 += a[j], sum2 += a[i];
